2ebb20f146a38e172309961c4c29979208ffe1eb
Luan Oliveira <luan.saldanha@compasso.com.br>
2024-08-12 18:32:25 -0300

    feat: javascript challenge 7 - magic numbers

---
+++ course/javascript/challenges/07-magic-numbers-solution.ts
@@ -0,0 +1,7 @@
+function lookForUpdades() {}
+
+const INTERVAL_30_MINUTES = 60 * 30 * 1000;
+
+setInterval(lookForUpdades, INTERVAL_30_MINUTES);
+
+function calculateDiscount(priceInCents, discountInPercentage) {}
