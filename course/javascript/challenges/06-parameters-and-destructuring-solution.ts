5c7bd8f3007b7a7e9d78fb66409c6959e799ea0b
Luan Oliveira <luan.saldanha@compasso.com.br>
2024-08-12 18:23:25 -0300

    feat: javascript challenge 6 - parameters and destructuring

---
+++ course/javascript/challenges/06-parameters-and-destructuring-solution.ts
@@ -0,0 +1,11 @@
+function updateUserRoute({ body, params }) {
+    updateUserController({ data: body, params })
+  }
+  
+  function updateUserController({ data, params}) {
+    userRepository.update({ data, params })
+  }
+  
+  const userRepository = {
+    update: ({data, params}) => {},
+  }
